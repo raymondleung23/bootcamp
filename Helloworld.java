feaf5f4d2ab49601586ce5ee4982cf7faf66a095
raymond.leung <raymondleung23@yahoo.com.hk>
2025-04-10 21:59:19 +0800

    fix bug 10.04.2025

---
+++ Helloworld.java
@@ -0,0 +1,8 @@
+public class Helloworld {
+  public static void main (String[] args) {
+    // I love java
+    // You good
+    // System.out.println is a tool
+    System.out.println("goodbye");
+  }
+}
